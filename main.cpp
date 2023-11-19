a62eb47d71075cd8cdb5c4d5f79422ef9b502ae5
night3098 <134074263+night3098@users.noreply.github.com>
2023-11-19 13:55:38 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow mainWindow;
+
     mainWindow.setStyleSheet( " background-color: #303030; " );
     mainWindow.show();
     return app.exec();
